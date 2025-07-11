c1a985fcb1876d137e12a5f212fc339e5ef98a65
Sanya  Bhatia <111678095+sanyaaaa14@users.noreply.github.com>
2025-07-11 12:40:52 +0530

    Create solution.java

---
+++ Recursion/Factorial /solution.java
@@ -0,0 +1,15 @@
+//GeeksForGeeks||Practise Solution 
+
+class Solution {
+
+    // Recursive function to calculate the factorial of a number
+    static int factorial(int n) {
+        // Base case: factorial of 1 is 1
+        if(n == 1){
+            return 1;
+        }
+
+        // Recursive case: multiply n with factorial of (n - 1)
+        return n * factorial(n - 1);
+    }
+}
